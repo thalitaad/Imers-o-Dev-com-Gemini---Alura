44e367f0716863e12a53add9a41e280f1fb5264f
thalitaad <damascenothalita2001@gmail.com>
2024-09-10 19:14:49 -0300

    add pratos

---
+++ dados.js
@@ -0,0 +1,67 @@
+let pratos = [
+    {
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBsJEBz8VVOniX8UMbqZK_kkC22-GjqYk9aw&s" ,
+        descricaoImagem: "Imagem de Açaí",
+        titulo: "Açaí",
+        descricao: "Fruto emblemático da Amazônia, o açaí é um ingrediente fundamental da culinária paraense. Tradicionalmente consumido como um pirão, o açaí é misturado com farinha de mandioca e pode ser acompanhado por peixe, camarão, carne ou outros ingrediente.",
+        link: "https://acaimineirinho.ind.br/glossario/como-o-acai-e-consumido-tradicionalmente-na-amazonia/#:~:text=Uma%20das%20maneiras%20mais%20tradicionais,com%20frutas%2C%20granola%20e%20mel.",
+        tags: "açai acai fruta"
+    },
+    {
+        imagem: "https://thumbs.dreamstime.com/b/prato-tradicional-brasileiro-de-marisco-caldeirada-do-norte-e-nordeste-gastronomia-brasileira-culin%C3%A1ria-246794928.jpg" ,
+        descricaoImagem: "Imagem de Calderada de Peixe",   
+        titulo: "Caldeirada de Peixe",
+        descricao: "Um ensopado feito com diversos tipos de peixe de água doce, temperado com urucum e outros ingredientes.",
+        link: "https://www.tudogostoso.com.br/receita/11313-caldeirada-paraense.html",
+        tags: ""
+    },
+    {
+        imagem: "https://www.saboresajinomoto.com.br/uploads/images/recipes/caruru-paraense.webp" ,
+        descricaoImagem: "Imagem de Caruru",    
+        titulo: "Caruru",
+        descricao: "Prato típico paraense, o caruru é um ensopado cremoso feito com quiabo, camarão seco, azeite de dendê e temperos especiais. Possui uma textura aveludada e um sabor marcante.",
+        link: "https://www.tudogostoso.com.br/receita/138495-caruru-paraense-simples.html",
+        tags: ""
+    },
+    {
+        imagem: "https://live.staticflickr.com/3833/33203150721_8b02b801d5_b.jpg" ,
+        descricaoImagem: "Imagem de maniçoba",
+        titulo: "Maniçoba",
+        descricao: "Considerada a feijoada paraense, a maniçoba é feita com folhas de mandioca brava cozidas por vários dias, carnes salgadas e defumadas, e temperos especiais.",
+        link: "https://www.tudogostoso.com.br/receita/155446-manicoba-simples-do-para.html",
+        tags: "manicoba"
+    },
+    {
+        imagem: "https://files.agro20.com.br/uploads/2020/04/pato-no-tucupi-2.jpg" ,
+        descricaoImagem: "Imagem de pato no tucupi",
+        titulo: "Pato no Tucupi",
+        descricao: "Um dos pratos mais famosos do Pará, feito com pato cozido no molho de tucupi, um caldo amarelo extraído da mandioca, e temperado com folhas de jambu, que proporcionam uma sensação de formigamento na boca.",
+        link: "https://www.tudogostoso.com.br/receita/155446-manicoba-simples-do-para.html",
+        tags: ""
+    },
+    {
+        imagem: "https://acozinhabrasileira.com.br/wp-content/uploads/2021/09/receita-de-tacaca.jpg" ,
+        descricaoImagem: "Imagem de Tacacá",
+        titulo: "Tacacá",
+        descricao: "De origem indígena. É preparado com tucupi, goma de tapioca, jambu e camarão seco. É um prato leve e refrescante.",
+        link: "https://www.tudogostoso.com.br/receita/49705-tacaca-original-do-para.html",
+        tags: "tacaca"
+    },
+    {
+        imagem: "https://media.gazetadopovo.com.br/2020/09/15174256/IMG_20200913_141511877b-960x540.jpg" ,
+        descricaoImagem: "Imagem de Tambaqui assado",    
+        titulo: "Tambaqui",
+        descricao: "Peixe muito popular na região, o tambaqui pode ser preparado de diversas formas, como assado, frito ou em moqueca.",
+        link: "https://www.tudogostoso.com.br/receita/62534-tambaqui-assado-e-recheado.html",
+        tags: ""
+    },
+    {
+        imagem: "https://live.staticflickr.com/7164/6700534351_be2a1cca0d_c.jpg" ,
+        descricaoImagem: "Imagem de Vatapá",
+        titulo: "Vatapá",
+        descricao: "Creme à base de camarão seco, farinha de trigo, leite de coco, azeite de dendê e temperos especiais. O vatapá paraense é diferente do baiano.",
+        link: "https://www.tudogostoso.com.br/receita/155543-vatapa-paraense.html",
+        tags: "vatapa"
+    },
+];
+
